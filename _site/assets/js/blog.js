52ac8777944a1f780315be0fe5572b2da68468bf
Mamadou Soumaila ali <lordp@kali>
2025-07-05 20:39:16 +0200

    Remove _site directory from repo

--- _site/assets/js/blog.js
+++
@@ -1,8 +0,0 @@
-// Dans assets/js/blog.js
-document.querySelector('.search-box button').addEventListener('click', function() {
-  const query = document.querySelector('.search-box input').value.toLowerCase();
-  document.querySelectorAll('.article-card').forEach(card => {
-    const title = card.querySelector('h2').textContent.toLowerCase();
-    card.style.display = title.includes(query) ? 'block' : 'none';
-  });
-});
